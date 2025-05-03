7ad02900fa1d758ce79cdfb9fa33ff778dda9dd9
Balkrishna Rawool <balkrishna.rawool@gmail.com>
2025-05-03 22:54:13 +0200

    Added model for services app

--- services/src/main/java/com/balarawool/bootloom/abank/services/domain/Model.java
+++ services/src/main/java/com/balarawool/bootloom/abank/services/domain/Model.java
@@ -1,13 +1,22 @@
 package com.balarawool.bootloom.abank.services.domain;
 
+<<<<<<< HEAD
 import java.util.List;
 
+=======
+>>>>>>> afdc184 (Added model for services app)
 public interface Model {
 
     record Customer(String id) { }
     record Account(String number, String balance) { }
     record Loan(String number, String amount) { }
+<<<<<<< HEAD
     record CreditScore(String score) { }
     record LoanOfferRequest(List<Account> accounts, List<Loan> loans, String creditScore, String amount, String purpose) { }
     record Offer(String id, String amount, String purpose, String interest, String offerText) { }
+=======
+    record LoanApplicationRequest(String amount, String purpose) { }
+    record Offer(String offerText) { }
+    record CreditScore(String score) { }
+>>>>>>> afdc184 (Added model for services app)
 }
