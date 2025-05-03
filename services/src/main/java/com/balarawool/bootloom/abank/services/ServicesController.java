ff96de2bbe13027f991a920136781edfbe54719e
Balkrishna Rawool <balkrishna.rawool@gmail.com>
2025-05-03 22:54:13 +0200

    Added model for services app

--- services/src/main/java/com/balarawool/bootloom/abank/services/ServicesController.java
+++ services/src/main/java/com/balarawool/bootloom/abank/services/ServicesController.java
@@ -1,5 +1,10 @@
 package com.balarawool.bootloom.abank.services;
 
+import com.balarawool.bootloom.abank.services.domain.Model.Account;
+import com.balarawool.bootloom.abank.services.domain.Model.CreditScore;
+import com.balarawool.bootloom.abank.services.domain.Model.Customer;
+import com.balarawool.bootloom.abank.services.domain.Model.Loan;
+import com.balarawool.bootloom.abank.services.domain.Model.Offer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +15,12 @@ import static com.balarawool.bootloom.abank.services.util.ThreadUtil.logAndWait;
 
 @RestController
 public class ServicesController {
-    record Customer(String id) { }
     @GetMapping("/current-customer")
     public Customer currentCustomer() {
         logAndWait("currentCustomer");
         return new Customer("1234");
     }
 
-    record CreditScore(String score) { }
     @GetMapping("/customer/{id}/credit-score1")
     public CreditScore getCreditScore1(@PathVariable("id") String customerId) {
         logAndWait("getCreditScore1");
@@ -30,24 +33,21 @@ public class ServicesController {
         return new CreditScore("Score2");
     }
 
-    public record Account(String number, String balance) { }
     @GetMapping("/customer/{id}/accounts")
     public List<Account> getAccountsInfo(@PathVariable("id") String customerId) {
         logAndWait("getAccountsInfo");
         return List.of(new Account("123", "1000.00"), new Account("456", "2000.00"));
     }
 
-    public record Loan(String number, String amount) { }
     @GetMapping("/customer/{id}/loans")
     public List<Loan> getLoansInfo(@PathVariable("id") String customerId) {
         logAndWait("getLoansInfo");
         return List.of(new Loan("TL123", "10000.00"), new Loan("CL456", "20000.00"));
     }
 
-    public record Offer(String offerText) { }
     @GetMapping("/customer/{id}/loans/offer")
     public Offer calculateOffer(@PathVariable("id") String customerId) {
         logAndWait("calculateOffer");
-        return new Offer("A new offer");
+        return new Offer("An offer for your loan application");
     }
 }
