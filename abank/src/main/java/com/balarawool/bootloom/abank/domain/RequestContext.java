58b6476be49189913de170a44717f3dfe7436dc7
Balkrishna Rawool <balkrishna.rawool@gmail.com>
2025-05-06 22:08:06 +0200

    Removed unused method

--- abank/src/main/java/com/balarawool/bootloom/abank/domain/RequestContext.java
+++ abank/src/main/java/com/balarawool/bootloom/abank/domain/RequestContext.java
@@ -5,8 +5,4 @@ import com.balarawool.bootloom.abank.domain.Model.Customer;
 public class RequestContext {
 
     public static final ScopedValue<Customer> CURRENT_CUSTOMER = ScopedValue.newInstance();
-
-    public static Customer getCurrentCustomer() {
-        return CURRENT_CUSTOMER.orElseThrow(() -> new Model.ABankException("No customer available"));
-    }
 }
